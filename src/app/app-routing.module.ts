6638637d842538a0488bb5044ee9bb93a095bc99
sebasrendon12 <srendon@lukount.com>
2022-01-09 18:52:14 -0500

    Ajustes panel de accesibilidad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { Pagina1Component } from './pagina1/pagina1.component';
 
 const routes: Routes = [
 
   { path: '', component: HomeComponent },
-  { path: 'pagina1', component: Pagina1Component },
   { path: 'home', component: HomeComponent },
 
 ];
